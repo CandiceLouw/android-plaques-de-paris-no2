a6d6357443d72f244df65556b8bf740026e6eca0
CandiceLouw <canlouw@gmail.com>
2020-05-29 09:51:50 +0200

    Include WebView component and individual clock widget xml code.

--- app/src/main/java/com/candicelouw/plaquesdeparisno2/MainActivity.java
+++ app/src/main/java/com/candicelouw/plaquesdeparisno2/MainActivity.java
@@ -2,12 +2,17 @@ package com.candicelouw.plaquesdeparisno2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView myWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myWebView = (WebView) findViewById(R.id.myWebView);
+        myWebView.loadUrl("file:///android_asset/plaquesdeparisno2.html");
     }
 }
---
+++ app/src/main/java/com/candicelouw/plaquesdeparisno2/paris_2e_basic.java
@@ -0,0 +1,67 @@
+package com.candicelouw.plaquesdeparisno2;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.widget.RemoteViews;
+
+public class paris_2e_basic extends AppWidgetProvider {
+
+    public void onReceive(Context context, Intent intent){
+        String action = intent.getAction();
+        PendingIntent pendingIntent;
+
+        if (AppWidgetManager.ACTION_APPWIDGET_UPDATE.equals(action)) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.paris_2e_basic);
+
+            PackageManager packageManager = context.getPackageManager();
+            Intent alarmClockIntent = new Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_LAUNCHER);
+
+            String clockImpls[][] = {
+                    { "HTC", "com.htc.android.worldclock",
+                            "com.htc.android.worldclock.WorldClockTabControl" },
+                    {"Standar Alarm Clock2", "com.google.android.deskclock",
+                            "com.android.deskclock.AlarmClock"},
+                    { "Standard", "com.android.deskclock",
+                            "com.android.deskclock.AlarmClock" },
+                    { "Froyo", "com.google.android.deskclock",
+                            "com.android.deskclock.DeskClock" },
+                    { "Motorola", "com.motorola.blur.alarmclock",
+                            "com.motorola.blur.alarmclock.AlarmClock" },
+                    { "Sony Ericsson Xperia Z", "com.sonyericsson.organizer",
+                            "com.sonyericsson.organizer.Organizer_WorldClock" },
+                    { "Sony Ericsson", "com.sonyericsson.alarm", "com.sonyericsson.alarm.Alarm" },
+                    { "Samsung", "com.sec.android.app.clockpackage",
+                            "com.sec.android.app.clockpackage.ClockPackage" } };
+
+            boolean foundClockImpl = false;
+
+            for(int i=0; i<clockImpls.length; i++)
+            {
+                String vendor = clockImpls[i][0];
+                String packageName = clockImpls[i][1];
+                String className = clockImpls[i][2];
+
+                try
+                {
+                    ComponentName cn = new ComponentName(packageName, className);
+                    alarmClockIntent.setComponent(cn);
+                    foundClockImpl = true;
+                }
+                catch (Exception e){};
+
+                if (foundClockImpl) {
+                    pendingIntent = PendingIntent.getActivity(context, 0, alarmClockIntent, 0);
+                    views.setOnClickPendingIntent(R.id.Widget, pendingIntent);
+                    AppWidgetManager.getInstance(context).updateAppWidget(intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS), views);
+                }
+            }
+        }
+
+    }
+
+}
